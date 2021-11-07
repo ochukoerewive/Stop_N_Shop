8256db99aa404ff2e070088439d4c62b9276de2c
ochukoerewive <ochukoerewive@gmail.com>
2021-11-07 06:07:14 +0000

    added form for vendors

--- rhodonitestore/urls.py
+++ rhodonitestore/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('vendors/', include('vendor.urls')),
 
 ]
--- vendor/views.py
+++ vendor/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def become_vendor(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form,save()
+
+            login(request, user)
+
+            vendor = Vendor.objects.create(name=user.username, created_by=user)
+
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'vendor/become_vendor.html', {'form': form})
