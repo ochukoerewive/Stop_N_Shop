472fa71ad0b771456119385c2e36327a4b1b38e5
ochukoerewive <ochukoerewive@gmail.com>
2021-11-07 23:43:27 +0000

    created vendor page, admin page and tested

--- vendor/urls.py
+++ vendor/urls.py
@@ -1,6 +1,11 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('become-vendor/', views.become_vendor, name='become_vendor')
+    path('become-vendor/', views.become_vendor, name='become_vendor'),
+    path('vendor-admin/', views.vendor_admin, name='vendor_admin'),
+
+   # path('logout/', views.logoutView.as_view(), name='logout'),
 ]
--- vendor/views.py
+++ vendor/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from .models import Vendor
@@ -20,3 +21,9 @@ def become_vendor(request):
         form = UserCreationForm()
 
     return render(request, 'vendor/become_vendor.html', {'form': form})
+
+@login_required
+def vendor_admin(request):
+    vendor = request.user.vendor
+
+    return render(request, 'vendor/vendor_admin.html', {'vendor': vendor})
