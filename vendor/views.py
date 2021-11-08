8d98f99ef7b39dc56d420adf8fd3f4eb6dff95f1
ochukoerewive <ochukoerewive@gmail.com>
2021-11-08 06:15:38 +0000

    creating list for the product in the admin area

--- vendor/views.py
+++ vendor/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from .models import Vendor
+from product.models import Product
 
 # Create your views here.
 def become_vendor(request):
@@ -25,5 +26,6 @@ def become_vendor(request):
 @login_required
 def vendor_admin(request):
     vendor = request.user.vendor
+    products = vendor.products.all()
 
-    return render(request, 'vendor/vendor_admin.html', {'vendor': vendor})
+    return render(request, 'vendor/vendor_admin.html', {'vendor': vendor, 'products': products})
