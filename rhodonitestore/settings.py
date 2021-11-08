b4e30fcefb947c535c63b2071b945ea1b15a9f5c
ochukoerewive <ochukoerewive@gmail.com>
2021-11-08 00:29:23 +0000

    created login and signup pages

--- rhodonitestore/settings.py
+++ rhodonitestore/settings.py
@@ -28,6 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'vendor_admin'
+LOGOUT_REDIRECT_URL = 'index'
 
 # Application definition
 
--- vendor/urls.py
+++ vendor/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('become-vendor/', views.become_vendor, name='become_vendor'),
     path('vendor-admin/', views.vendor_admin, name='vendor_admin'),
 
-   # path('logout/', views.logoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='vendor/login.html'), name='login'),
 ]
