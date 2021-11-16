64e60809fc78187a3272c6e1e4ef4367e39e43b5
ochukoerewive <ochukoerewive@gmail.com>
2021-11-16 05:40:21 +0000

    added product and form app to view in front end

--- vendor/views.py
+++ vendor/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.text import slugify
 from django.shortcuts import render, redirect
 
 from .models import Vendor
 from product.models import Product
+from .forms import ProductForm
 
 # Create your views here.
 def become_vendor(request):
@@ -26,6 +28,25 @@ def become_vendor(request):
 
 @login_required
 def vendor_admin(request):
-    vendor = request.user
+    vendor = request
+    products = vendor
 
-    return render(request, 'vendor/vendor_admin.html', {'vendor': vendor})
+    return render(request, 'vendor/vendor_admin.html', {'vendor': vendor, 'products': products})
+
+
+@login_required
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+
+        if forms.is_valid():
+            product = form.save(commit=False)
+            product.vendor = request.user.vendor
+            product.slug = slugify(product.title)
+            product.save()
+
+            return redirect('vendor_admin')
+    else:
+        form = ProductForm()
+
+    return render(request, 'add_product.html', {'form': form})
