f5784bdb8af400ebafb83142797da3c27511fc72
ochukoerewive <ochukoerewive@gmail.com>
2021-11-17 06:35:12 +0000

    started styling

--- core/views.py
+++ core/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
+from product.models import Product
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    newest_products = Product.objects.all()[0:8]
+    
+    return render(request, 'index.html', {'newest_products': newest_products})
 
 def contact(request):
     return render(request, 'contact.html')
