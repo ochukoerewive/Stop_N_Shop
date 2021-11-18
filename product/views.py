7e76f33017e509e61ce8d4d46e9cc821375d8538
ochukoerewive <ochukoerewive@gmail.com>
2021-11-18 06:31:57 +0000

    created product.html

--- product/views.py
+++ product/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+import random
+
+from django.shortcuts import render, get_object_or_404
+
+from .models import Category, Product
 
 # Create your views here.
+def product(request, category_slug, product_slug):
+    product = get_object_or_404(Product, category__slug=category_slug, slug=product_slug)
+
+    similar_products = list(product.caregory.products.exclude(id=product.id))
+
+    if len(similar_products) >= 4:
+        similar_products = random.sample(similar_products, 4)
+
+    return render(request, 'product/product.html', {'product': product, 'similar_products': similar_products})
