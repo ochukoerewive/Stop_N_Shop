ede3610d5f9559125368ca3300f1fca9169947a3
ochukoerewive <ochukoerewive@gmail.com>
2021-11-21 16:11:16 +0000

    editing base.html and the navbar

--- product/views.py
+++ product/views.py
@@ -1,8 +1,15 @@
 import random
 
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 
 from .models import Category, Product
+
+def search(request):
+    query = request.GET.get('query', '')
+    products = Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'search.html', {'products': products, 'query': query})
 
 def product(request, category_slug, product_slug):
 
