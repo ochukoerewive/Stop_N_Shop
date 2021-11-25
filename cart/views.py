f308da06fc2d68a088f450d9b21da81a59eaae15
ochukoerewive <ochukoerewive@gmail.com>
2021-11-25 18:54:13 +0000

    added delete function in cart.views.py

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .cart import Cart
 
 # Create your views here.
 def cart_detail(request):
+    cart = Cart(request)
+    remove_from_cart = request.GET.get('remove_from_cart', '')
+
+    if remove_from_cart:
+        cart.remove(remove_from_cart)
+
+        return redirect('cart')
+
     return render(request, 'cart/cart.html')
