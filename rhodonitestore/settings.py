5179d190e79fbc772d9efa546d874ea9a561b51b
ochukoerewive <ochukoerewive@gmail.com>
2022-01-05 08:21:33 +0000

    Removed secret key and set debug

--- rhodonitestore/settings.py
+++ rhodonitestore/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m_9r*a$7o!mir^t40!=lv!i$ixd-h!)=id^(s53d2do@55=g8a'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['rhodonitestore.herokuapp.com','localhost']
 
