8ca56670818ab023620a0e150069c7b4b0937f8b
ochukoerewive <ochukoerewive@gmail.com>
2022-01-09 07:46:37 +0000

    Working on the interface

--- rhodonitestore/settings.py
+++ rhodonitestore/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'j2yekzi4)mk+w3dh755#0s_c*ej@e_&grp$*#2xz3a)%%0-yvj')
+#SECRET_KEY = os.environ.get('SECRET_KEY', 'j2yekzi4)mk+w3dh755#0s_c*ej@e_&grp$*#2xz3a)%%0-yvj')
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
