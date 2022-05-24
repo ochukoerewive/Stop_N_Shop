d8adc12e8aedf8d0fbf05257cff423806092e15a
ochukoerewive <ochukoerewive@gmail.com>
2022-05-24 23:39:40 +0000

    56 deployment part 2

--- rhodonitestore/settings.py
+++ rhodonitestore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['admin-rhodonitestore.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['rhodonitestore.herokuapp.com', 'localhost']
 
 STRIPE_PUB_KEY = os.environ.get('STRIPE_PUB_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
